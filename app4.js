43f07b8f5e430e7a81bb7d32a78fb63bf2c7d285
Ezequiel <ezequielmaggi@gmail.com>
2021-01-13 00:44:33 -0300

    principio de vue

---
+++ app4.js
@@ -0,0 +1,8 @@
+var app4 = new Vue({
+  el: "#app-4",
+  data: [
+    { text: "Aprender JavaScript" },
+    { text: "Aprender Vue" },
+    { text: "Construir algo increible" },
+  ],
+});
--- main.js
+++ main.js
@@ -4,3 +4,48 @@ var app = new Vue({
     message: "Hola Vue!",
   },
 });
+
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    message: "Usted cargo esta pagina el " + new Date().toLocaleString(),
+  },
+});
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true,
+  },
+});
+app3.seen = true;
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [
+      { text: "Aprender JavaScript" },
+      { text: "Aprender Vue" },
+      { text: "Construir algo increíble" },
+    ],
+  },
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Hola Vue.js!",
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "Hola Vue!",
+  },
+});
